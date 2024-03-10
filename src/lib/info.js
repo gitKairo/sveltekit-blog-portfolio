1de9374e4ad123a6f151d91253cc8fc13e16c95c
Kai Ruge <kairuge@hotmail.com>
2024-03-10 15:57:33 -0500

    First Post, Updated info.js

--- src/lib/info.js
+++ src/lib/info.js
@@ -3,25 +3,23 @@
 // feel free to remove this file.
 
 // URL of your website
-export const website = 'https://sveltekit-blog-template.vercel.app'
+export const website = 'kairuge.dev'
 
-export const firstName = 'Jordan'
-export const lastName = 'Doe'
+export const firstName = 'Kai'
+export const lastName = 'Ruge'
 export const name = `${firstName} ${lastName}`
 
 // Used for the landing page & footer of posts
-export const avatar = 'https://picsum.photos/id/203/250/250'
+export const avatar = 'https://picsum.photos/id/29/250/250'
 export const bio = `\
-I’m ${firstName}, a software engineer based in Canada. Lorem ipsum dolor sit amet, consectetur
-adipiscing elit. Phasellus accumsan quam at eros malesuada, id luctus lectus efficitur. Nulla
-sit amet arcu mi. Duis quis mauris malesuada, posuere orci et, pulvinar nisl. Nam malesuada
-vehicula sapien vitae sagittis.
+I’m ${firstName}, a Software Engineer based in Springfield, MO. As of now I specialize in full-stack devlopment
+and have been working as the sole Web Developer at a marketing agency for the past year and a half.
 `
 
 // Your social usernames, empty strings won't be used.
 // For sake of the live demo, I'm using `username/repo` for github,
 // but you can replace with just your username.
-export const github = 'mattjennings/sveltekit-blog-template'
+export const github = 'gitKairo'
 export const twitter = ''
-export const linkedin = ''
+export const linkedin = '/in/kai-ruge-49526319b'
 export const instagram = ''
