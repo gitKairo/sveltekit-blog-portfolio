182d92c07cbe6e1274665bdca705c1617153a1f2
Kai Ruge <kairuge@hotmail.com>
2024-03-10 17:13:42 -0500

    Added custom domain URL to info.js

--- src/lib/info.js
+++ src/lib/info.js
@@ -3,7 +3,7 @@
 // feel free to remove this file.
 
 // URL of your website
-export const website = 'https://sveltekit-blog-template.vercel.app'
+export const website = 'https://www.kairuge.dev/'
 
 export const firstName = 'Kai'
 export const lastName = 'Ruge'
